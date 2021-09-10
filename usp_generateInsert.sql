9d9376e10ed2155365fe8f7b319ac8a29e9edbba
Menkar Cantor <menkar91@gmail.com>
2021-09-10 12:41:19 -0500

    Update usp_generateInsert.sql
    Ajustes:
    - Se corrige problema con campos tipo fecha
    - Se cambia @filter por @where para mayor comprensión
    
    Mejoras:
    - Se agrega @select que permite definir que campos quieren incluir en el insert

--- usp_generateInsert.sql
+++ usp_generateInsert.sql
@@ -1,7 +1,8 @@
 create or alter procedure usp_generateInsert (
     @table  varchar(max),
-    @filter varchar(max)='',
     @mode   varchar(20)='values',
+    @select varchar(max)='',
+    @where  varchar(max)='',
     @order  varchar(max)=''
 )
 as
@@ -21,8 +22,11 @@ declare
     @result     varchar(max),
     @firstField varchar(max)
 begin
-    if @filter<>'' and @filter not like '%where%'
-        set @filter='where '+@filter
+    if @select<>''
+        set @select=replace(@select,', ',',')
+    
+    if @where<>'' and ltrim(@where) not like 'where%'
+        set @where='where '+@where
     
     if @order<>'' and @order not like '%order by%'
         set @order='order by '+@order
@@ -41,6 +45,7 @@ begin
         where object_id=object_id(@table) 
             and is_identity!=1 
             and is_computed!=1
+            and 1=iif(coalesce(@select,'')='',1,iif((select count(1) from string_split(@select,',') a where a.value=name)=0,0,1))
         for xml path('')
     ),1,1,'')
 
@@ -49,6 +54,7 @@ begin
             (case 
                 when t.name in ('image','text') then '''null'''
                 when t.precision!=0 and (t.name not like '%date%' and t.name not like '%time%') then 'convert(varchar('+convert(varchar(10),iif(c.precision>=4,c.precision,4))+'),'+c.name+')'
+                when t.name like '%date%' and t.name like '%time%' then '''''''''+replace(convert(varchar,'+c.name+','+(case t.name when 'date' then '112' when 'time' then '24' else '21' end)+'),''-'','''')+'''''''''
                 --Revisar cuando son bit y tienen null solo toma la primera letra, osea n en vez de null
                 else '''''''''+'+c.name+'+'''''''''
             end)
@@ -58,13 +64,14 @@ begin
          where c.object_id=object_id(@table)
             and c.is_identity!=1 
             and c.is_computed!=1
+            and 1=iif(coalesce(@select,'')='',1,iif((select count(1) from string_split(@select,',') a where a.value=c.name)=0,0,1))
          for xml path('')
     ),1,7,'')
 
     if @mode='values'
     begin
         select @field=replace(@field,'||','')
-        set @result='select ''insert into '+@table+'('+@columns+')values('''+'+'+@field+'+'+''')'''+' from '+@table+' '+@filter+' '+@order
+        set @result='select ''insert into '+@table+'('+@columns+')values('''+'+'+@field+'+'+''')'''+' from '+@table+' '+@where+' '+@order
     end
     else if @mode='select'
     begin
@@ -73,7 +80,7 @@ begin
         set @result='select ''--insert into '+@table+'('+@columns+')'''+char(13)
                    +'union all select iif(row_number() over (order by '+@firstField+')=1,'''',''union all '')+''select '''+'+'+char(13)
                    +'    '+@field+char(13)
-                   +'from '+@table+' '+@filter+' '+@order
+                   +'from '+@table+' '+@where+' '+@order
     end
     else if @mode='valuesTotal'
     begin
@@ -84,7 +91,7 @@ begin
                    +'union all select iif(row_number() over (order by '+@firstField+')=1,'' '','','')+''('''+'+'+char(13)
                    +'    '+@field+char(13)
                    +'+'+''')'''+char(13)
-                   +'from '+@table+' '+@filter+' '+@order
+                   +'from '+@table+' '+@where+' '+@order
     end
     else
     begin
